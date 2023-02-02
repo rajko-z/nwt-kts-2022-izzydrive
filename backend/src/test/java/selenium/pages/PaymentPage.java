7f808d03edb20b89cc9f4ea9dfcaf31cfd025ee1
nevena <prokic.sw6.2019@uns.ac.rs>
2023-02-02 22:59:29 +0100

    Zavrseni selenijum testovi za narucivanje voznje

---
+++ backend/src/test/java/selenium/pages/PaymentPage.java
@@ -0,0 +1,44 @@
+package selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PaymentPage {
+
+    private WebDriver driver;
+
+    @FindBy(css = ".submit-button-approve-payment")
+    WebElement paymentButton;
+
+    @FindBy(css = ".submit-button-cancel-payment")
+    WebElement cancelButton;
+
+    @FindBy(css = ".text.title")
+    WebElement titleElement;
+
+    public PaymentPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnPayButton() {
+        paymentButton.click();
+    }
+
+    public void clickOnCancelButton() {
+        cancelButton.click();
+    }
+
+    public boolean isOpened() {
+        return (new WebDriverWait(driver, Duration.ofSeconds(10)))
+                .until(ExpectedConditions.textToBePresentInElement(titleElement, "To pay:"));
+    }
+
+
+}
